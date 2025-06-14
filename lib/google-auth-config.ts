765ceb9fe186621b198ca11b8d0ff9ccdeac0273
ITzmeMod <fordfortuito@gmail.com>
2025-06-14 11:46:08 +0800

    Fix Google OAuth configuration and add setup validation

--- lib/google-auth-config.ts
+++ lib/google-auth-config.ts
@@ -1,5 +1,5 @@
 // This file contains the configuration for Google OAuth
 // You'll need to create a project in the Google Developer Console and get these credentials
 
-export const GOOGLE_CLIENT_ID = "YOUR_ACTUAL_GOOGLE_CLIENT_ID" // You'll get this from Google Cloud Console
+export const GOOGLE_CLIENT_ID = "1039114733186-g6t8o74124davbf2v4cud6ldtjfvo9gi.apps.googleusercontent.com" // You'll get this from Google Cloud Console
 export const OWNER_EMAIL = "fortuitocliffordgwapo@gmail.com" // Owner's Google email address
